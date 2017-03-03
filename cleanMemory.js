7fd0062bf64f8fae4791e741a4a60d51af68b3d7
liudo <hjyue1@163.com>
2017-03-03 09:19:09 +0800

    清除内存

--- cleanMemory.js
+++ cleanMemory.js
@@ -2,7 +2,9 @@ const spawn = require('child_process').spawn;
 
 //调用系统命令清理内存
 (function cleanMemory() {
-    const clean = spawn('echo', ['1', '>', '/proc/sys/vm/drop_caches']);
+    const clean = spawn('echo', ['1', '>', '/proc/sys/vm/drop_caches'], {
+        shell: true
+    });
     // 捕获标准输出并将其打印到控制台 
     clean.stdout.on('data', function(data) {
         console.log('standard output:\n' + data);
